c11bd88f6ecf31791a91a89495d8069c955c8861
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-12-14 19:52:31 +0100

    moteur en cours

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,31 +1,27 @@
-var $ = require('jquery');
-require('bootstrap-sass');
-require('../css/global.scss');
-
-$(document).ready(function () {
-    $("#appbundle_room_object").keyup(function () {
-        const inputObject = $(this).val().toLowerCase();
-        if (inputObject.length >= 2) {
+$( document ).ready(function() {
+    $("#appbundle_room_object").keyup(function(){
+        alert('ifzef');
+        const inputObject = $(this).val().toUpperCase();
+        if ( inputObject.length >= 2 ) {
             $.ajax({
                 type: "POST",
                 url: "/object/list/" + inputObject,
                 dataType: 'json',
                 timeout: 3000,
-                success: function (response) {
+                success: function(response){
                     const objects = JSON.parse(response.data);
                     let html = "";
                     for (object of objects) {
                         let highlightObject = object.object.replace(inputObject, `<span class="highlight">${inputObject}</span>`);
-                        html += `<li class="list-group-item"><span class="object">${highlightObject}</span></li>`;
-                    }
+                        html += `<li class="list-group-item"><span class="object">${highlightObject}</span></li>`;                    }
                     $('#autocomplete').html(html);
-                    $('#autocomplete li').on('click', function () {
+                    $('#autocomplete li').on('click', function() {
                         let getObject = $(this).children('span').first().text();
                         $('#appbundle_room_object').val(getObject);
                         $('#autocomplete').html('');
                     });
                 },
-                error: function () {
+                error: function() {
                     $('#autocomplete').text('Ajax call error');
                 }
             });
