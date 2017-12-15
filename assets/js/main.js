d0aef940fa365a68a02e6234162eb7f242b41b92
Thibaut Decherit <thibaut.decherit@gmail.com>
2017-12-15 10:13:13 +0100

    chargé dans la vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,3 @@
 var $ = require('jquery');
 require('bootstrap-sass');
 require('../css/global.scss');
-
-$(document).ready(function () {
-    $("#appbundle_room_object").keyup(function () {
-        const inputObject = $(this).val().toLowerCase();
-        if (inputObject.length >= 2) {
-            $.ajax({
-                type: "POST",
-                url: "/search/objects/list/" + inputObject,
-                dataType: 'json',
-                timeout: 3000,
-                success: function (response) {
-                    const objects = JSON.parse(response.data);
-                    let html='';
-                    for (object of objects) {
-                        html += `<li data-id="${object.id}">${object.name}</li>`;
-                    }
-                    $('#autocomplete').html(html);
-                },
-                error: function () {
-                    $('#autocomplete').text('Ajax call error');
-                }
-            });
-        }
-    });
-});
